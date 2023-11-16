13fd3157e1bf9aba043b2638396fa2e59031d928
murillocjr <murillo.cjr@gmail.com>
2023-11-16 08:59:21 -0500

    fix_data

--- marvel/views.py
+++ marvel/views.py
@@ -25,6 +25,6 @@ class Login(APIView):
         serializer = LoginRequestSerializer(data=request.data)
         if serializer.is_valid():
             if serializer.data["username"] == "admin" and serializer.data["password"] == "123456":
-                return Response({"status": "success", "code": 200, "data": { "username": "user-user", "devicename": "android", "genericname": "demo"} }, status=status.HTTP_200_OK, content_type="application/json")
+                return Response({"status": "success", "code": 200, "data": {"id":"007", "username": "bond-james", "firstname": "james", "lastname": "bond"} }, status=status.HTTP_200_OK, content_type="application/json")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
