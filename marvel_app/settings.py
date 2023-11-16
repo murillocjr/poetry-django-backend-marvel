aecdc2f14b1ec163b4f67ade0e9047867a0a3bd2
murillocjr <murillo.cjr@gmail.com>
2023-11-16 08:36:23 -0500

    allowed_hosts

--- marvel_app/settings.py
+++ marvel_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xl*_%&@-%sd-(05p+rh*lj7)a(a2qu6jph^f8ly)37a5g2)29$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
